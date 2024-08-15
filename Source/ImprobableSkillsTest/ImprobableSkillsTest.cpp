1db0e2d4784dcdd29b9915a7e04b41d888d180c6
LewisRollins <48557652+LewisRollins@users.noreply.github.com>
2024-08-15 19:10:04 +0100

    Initial commit

---
+++ Source/ImprobableSkillsTest/ImprobableSkillsTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ImprobableSkillsTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ImprobableSkillsTest, "ImprobableSkillsTest" );
